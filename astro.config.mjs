32c12409fd18a0ee4da07f1618a4aba3b6291863
Alberto Gallego <albertogc2793@gmail.com>
2025-09-15 15:55:16 +0200

    canonical error fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   site: "https://picmal.app",
+  trailingSlash: "never",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],

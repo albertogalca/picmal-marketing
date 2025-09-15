2066d8eafb1f90d0f9a1f7dbc531f04cfb67b197
Alberto Gallego <albertogc2793@gmail.com>
2025-09-15 14:43:21 +0200

    Replace with new Astro marketing site

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://picmal.app",
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: "static",
+  adapter: vercel({
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+    },
+  }),
+});
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Picmal";
+export const SITE_DESCRIPTION =
+  "Picmal is a privacy-first image converter for Mac. It works entirely offline and lets you convert multiple images at once. Supports 20+ formats including JPEG, PNG, WEBP, AVIF, HEIC, RAW, TIFF, PSD, and more.";

5c89ff30045b1721418d3bc157dfa8af6dc39b23
Alberto Gallego <albertogc2793@gmail.com>
2025-12-02 13:57:28 +0100

    trusted by added

---
+++ src/utils/gravatar.ts
@@ -0,0 +1,9 @@
+import crypto from "crypto";
+
+export function getGravatarUrl(email: string, size: number = 80): string {
+  const hash = crypto
+    .createHash("md5")
+    .update(email.toLowerCase().trim())
+    .digest("hex");
+  return `https://gravatar.com/avatar/${hash}?s=${size}&d=mp&r=g`;
+}

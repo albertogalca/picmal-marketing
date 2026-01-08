e66d92111de9a0edbec8b98bc67ef5189a8597e1
Alberto Gallego <albertogc2793@gmail.com>
2026-01-08 13:29:44 +0100

    changelog out of the blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,4 +17,13 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const changelog = defineCollection({
+	loader: glob({ base: './src/content/changelog', pattern: '**/*.{md,mdx}' }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+	}),
+});
+
+export const collections = { blog, changelog };

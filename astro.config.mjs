2e7e135ca52d3ffe79ac4c680ff82d8bafe509fb
Alberto Gallego <albertogc2793@gmail.com>
2026-01-27 14:07:29 +0100

    simplified landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from "@astrojs/sitemap";
 import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import starlightThemeBlack from "starlight-theme-black";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://picmal.app",
   trailingSlash: "never",
@@ -18,47 +18,24 @@ export default defineConfig({
       logo: {
         src: "./public/logo.webp",
         alt: "Picmal",
-        replacesTitle: true,
       },
-      defaultLocale: "root",
-      locales: {
-        root: {
-          label: "English",
-          lang: "en",
-        },
-      },
-      social: [
-        {
-          icon: "github",
-          label: "Github",
-          href: "https://github.com/albertogalca/picmal-releases",
-        },
-        {
-          icon: "twitter",
-          label: "Twitter",
-          href: "https://x.com/picmalapp",
-        },
-      ],
       sidebar: [
         {
           label: "Quick start",
-          items: [
-            { label: "Introduction", link: "/docs/" },
-            {
-              label: "Installation",
-              link: "/docs/getting-started/installation/",
-            },
-            {
-              label: "Quick Start",
-              link: "/docs/getting-started/quick-start/",
-            },
-          ],
+          items: [{ label: "Introduction", link: "/docs" }],
         },
       ],
-      components: {
-        Head: "./src/components/starlight/Head.astro",
-        ThemeProvider: "./src/components/starlight/ThemeProvider.astro",
-      },
+      plugins: [
+        starlightThemeBlack({
+          navLinks: [
+            {
+              label: "Docs",
+              link: "/getting-started",
+            },
+
+          ],
+        }),
+      ],
     }),
     mdx(),
     sitemap(),
